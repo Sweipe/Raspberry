e5dc31d7c577a9dcb2cd61ff20eff4f3ecd83890
Sweipe <aled@hotmail.se>
2026-05-13 16:34:12 +0200

    forgot the sensor.py

---
+++ sensor.py
@@ -0,0 +1,15 @@
+import explorerhat
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = PiGPIOFactory(host='raspberrypi.local')
+
+
+import gpiozero
+from gpiozero import DistanceSensor
+
+echo_pin = 7
+trig_pin= 12
+sensor = DistanceSensor(echo=echo_pin, trigger=trig_pin, pin_factory=factory)
+
+while True:
+    print('Distance: 'sensor.distance*100)
