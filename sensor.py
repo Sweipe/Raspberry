4d6b64402f820dcabca3a44e700276068c11f54c
Sweipe <aled@hotmail.se>
2026-05-13 16:42:38 +0200

    6

--- sensor.py
+++ sensor.py
@@ -10,7 +10,7 @@ from gpiozero import DistanceSensor
 
 echo_pin = 7
 trig_pin= 12
-sensor = DistanceSensor(echo=echo_pin, trigger=trig_pin, pin_factory=factory)
+sensor = DistanceSensor(echo=echo_pin, trigger=trig_pin)
 
 while True:
     print('Distance: ',sensor.distance*100)
